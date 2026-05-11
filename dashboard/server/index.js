035cf8012f8a2a3a5ce760a7baebaa8b2595cd1c
ViperFoxTV <viperfox1738@outlook.com>
2026-05-11 06:26:51 -0700

    Update index.js

--- dashboard/server/index.js
+++ dashboard/server/index.js
@@ -19,11 +19,8 @@ dotenv.config();
 // DATABASE CONNECTION
 // ==============================
 export const db = new pg.Pool({
-  host: process.env.POSTGRES_HOST,
-  user: process.env.POSTGRES_USER,
-  password: process.env.POSTGRES_PASSWORD,
-  database: process.env.POSTGRES_DB,
-  port: process.env.POSTGRES_PORT || 5432,
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false },
 });
 
 const app = express();
